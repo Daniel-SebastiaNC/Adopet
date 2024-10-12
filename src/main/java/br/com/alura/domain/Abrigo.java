187bc8eb43a3df2a705f1cfb45fe05635bbc33dd
Daniel Sebastian Nunes Cavalcanti <daniel.sebastian.nc@gmail.com>
2024-10-11 21:28:45 -0300

    tests

--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -35,4 +35,15 @@ public class Abrigo {
     public String getTelefone() {
         return telefone;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                     "id":%s,"nome":"%s","telefone":"%s","email":"%s"
+                     """.formatted(this.id, this.nome, this.telefone, this.email);
+    }
 }
